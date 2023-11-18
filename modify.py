1841af225836478a384ffe5c0629d1028abd9088
JoJo0217 <mingu0217@gmail.com>
2023-11-18 15:35:33 +0900

    add step1 train3 data and add step2 square

--- modify.py
+++ modify.py
@@ -1,19 +1,25 @@
 import json
 import jsonlines
 data=[]
-file_name='./step1/train/train.jsonl'
+file_name='./step1/train3/raw/yitingxie_non_list.jsonl'
 save_name=file_name
 
 with open(file_name, "r", encoding="utf-8") as file:
     for line in file:
         data.append( json.loads(line))
 
-result=[]
 for i in data:
-    if 'category' in i:
-        del i['category']     
-
+    if 'prompt' in i:
+        i['instruction']=i['prompt']
+        del i['prompt']     
+    if 'input' not in i:
+        i['input']=""
+    if 'chosen' in i:
+        i['output']=i['chosen']
+        del i['chosen']
+    if 'rejected' in i:
+        del i['rejected']
 
 with jsonlines.open(save_name, mode='w') as writer:
-    for one_data in result:
+    for one_data in data:
         writer.write(one_data)
